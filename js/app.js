114b5ee0fd97f1bc30ff287ca2f5dc145d73709c
ovan zaimul <ovanzaimul@gmail.com>
2021-12-05 22:04:57 +0800

    comment console.log

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
-const nav = document.querySelector('.nav');
-const header = document.querySelector('.header');
-const body = document.querySelector('body');
-const loaderEl = document.querySelector('#loader');
-console.log(loaderEl);
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const body = document.querySelector("body");
+const loaderEl = document.querySelector("#loader");
+// console.log(loaderEl);
 
-console.log(header);
+// console.log(header);
 
 const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 1.0,
+	root: null,
+	rootMargin: "0px",
+	threshold: 1.0,
 };
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      nav.classList.add('nav__background');
-    } else {
-      nav.classList.remove('nav__background');
-    }
-  });
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			nav.classList.add("nav__background");
+		} else {
+			nav.classList.remove("nav__background");
+		}
+	});
 }, options);
 
 observer.observe(header);
 
-particlesJS.load('particles-js', 'js/particles.json', function () {
-  console.log('particle is loaded....😎😎');
-  console.log('callback - particles.js config loaded');
+particlesJS.load("particles-js", "js/particles.json", function () {
+	// console.log('particle is loaded....😎😎');
+	// console.log('callback - particles.js config loaded');
 });
 
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    loaderEl.classList.add('hide');
-    loaderEl.classList.remove('show');
-  }, 1000);
+window.addEventListener("load", () => {
+	setTimeout(() => {
+		loaderEl.classList.add("hide");
+		loaderEl.classList.remove("show");
+	}, 1000);
 });
